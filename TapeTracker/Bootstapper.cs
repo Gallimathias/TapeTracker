5bc61c31d5cc1db7fe26f377b08649d42f20dcf1
Maximilian Krüger <maximilian.krueger@it.konicaminolta.de>
2018-06-10 18:13:18 +0200

    Add Projects to sln

---
+++ TapeTracker/Bootstapper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using Prism.DryIoc;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TapeTracker
+{
+    internal class Bootstrapper : DryIocBootstrapper
+    {
+        protected Application CurrentApplication => Application.Current;
+        protected BindableBase ShellBase => (BindableBase)CurrentApplication.MainWindow.DataContext;
+
+        protected override DependencyObject CreateShell()
+            => ServiceLocator.Current.GetInstance<MainWindow>();
+
+        protected override void InitializeShell()
+        {
+            //PageContainer.RegisterAssembly(Assembly.GetExecutingAssembly());
+
+            //ShellBase.CollectPages();
+
+            CurrentApplication.MainWindow = (MainWindow)Shell;
+            CurrentApplication.MainWindow.Show();
+        }
+
+    }
+}
