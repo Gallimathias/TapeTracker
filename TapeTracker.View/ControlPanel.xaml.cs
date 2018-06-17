aded6ee91300d181e1beee81f4c0920f376d8708
Maximilian Krüger <maximilian.krueger@it.konicaminolta.de>
2018-06-17 15:07:27 +0200

    Work on first ui stub

---
+++ TapeTracker.View/ControlPanel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TapeTracker.View
+{
+    /// <summary>
+    /// Interaktionslogik für ControlPanel.xaml
+    /// </summary>
+    public partial class ControlPanel : Page
+    {
+        public ControlPanel()
+        {
+            InitializeComponent();
+        }
+    }
+}
